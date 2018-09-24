5565e84d73273f548cfc8d0908968ca892968227
chy <1260147774@qq.com>
2018-09-24 16:50:53 +0800

    add class_trend

--- class_trend.py
+++ class_trend.py
@@ -1,9 +1,31 @@
 import db_operation as dbo
+import matplotlib.pyplot as plt
 
-for i in range(4,15):
-    x = dbo.select_where('Date', 'class_trend', "ClassId='i'")
-    y = dbo.select_where('NewPick', 'class_trend', "ClassId='i'")
-    z = dbo.select_where('NewDrop', 'class_trend', "Class_id='i'")
+for i in range(4,14):
+    x = dbo.select_where('CAST(date AS date) ', 'class_trend', "ClassId='"+str(i)+"'")
+    y = dbo.select_where('CAST(NewPick AS signed)', 'class_trend', "ClassId='"+str(i)+"'")
+    z = dbo.select_where('CAST(NewDrop AS signed)', 'class_trend', "ClassId='"+str(i)+"'")
+
+    # print(x)
+    # print(y)
+    # print(z)
+
+    plt.plot(x,y,"g-")
+    plt.plot(x,z,"r-")
+
+    plt.ylim(0,2400)
+
+    plt.xlabel("date")
+    plt.ylabel("number of students")
+
+    plt.title("NewPick Student Number and Total Student Number"+" of Class"+str(i))
+
+    plt.savefig("class_trend" + str(i))
+    plt.show()
+    print(type(x))
+
+
+
 
 
 
