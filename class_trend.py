98f3ac186019e422e183e09c1788d9c34e3c2190
chy <1260147774@qq.com>
2018-10-10 16:46:41 +0800

    change class_trend

--- class_trend.py
+++ class_trend.py
@@ -5,25 +5,30 @@ plt.rcParams['figure.figsize'] = (12.0, 4.0)
 for i in range(4,14):
     x = dbo.select_where('CAST(date AS date) ', 'class_trend', "ClassId='"+str(i)+"'")
     y = dbo.select_where('CAST(NewPick AS signed)', 'class_trend', "ClassId='"+str(i)+"'")
-    z = dbo.select_where('CAST(NewDrop AS signed)', 'class_trend', "ClassId='"+str(i)+"'")
-
-    # print(x)
-    # print(y)
-    # print(z)
 
     plt.plot(x,y,"g-")
-    plt.plot(x,z,"r-")
-
-    plt.ylim(0,2400)
-
+    plt.ylim(0, 2400)
     plt.xlabel("date")
     plt.ylabel("number of students")
+    plt.title("Total Student Number" + " of Class" + str(i))
 
-    plt.title("NewPick Student Number and Total Student Number"+" of Class"+str(i))
+plt.show()
+print(type(x))
+
+for i in range(4,14):
+    x = dbo.select_where('CAST(date AS date) ', 'class_trend', "ClassId='" + str(i) + "'")
+    z = dbo.select_where('CAST(NewDrop AS signed)', 'class_trend', "ClassId='" + str(i) + "'")
+
+    plt.plot(x,z,"r-")
+    plt.ylim(0, 100)
+    plt.xlabel("date")
+    plt.ylabel("number of students")
+    plt.title("NewPick Student Number" + " of Class" + str(i))
 
     plt.savefig("class_trend_result/class_trend" + str(i))
-    plt.show()
-    print(type(x))
+
+plt.show()
+print(type(x))
 
 
 
